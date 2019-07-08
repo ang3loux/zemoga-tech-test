7a3f3dfbd312033a69a6fc6d8592a3e1e3e352f4
Angelo Zambrano <angelo.zambrano.1990@gmail.com>
2019-07-08 10:56:27 -0500

    now when a post is deleted, the card closes

--- App/Containers/Home/Components/PostCard/index.js
+++ App/Containers/Home/Components/PostCard/index.js
@@ -4,23 +4,39 @@ import { PropTypes } from 'prop-types'
 import styles from './styles'
 
 class Component extends React.PureComponent {
+  constructor(props) {
+    super(props)
+    this.cardRef = null
+  }
+
+  onDeletePost() {
+    const { onDeletePost } = this.props
+    this.cardRef._root.closeRow()
+    setTimeout(() => onDeletePost(), 500)
+  }
+
   render() {
-    const { wasRead, body, isFavorite } = this.props
+    const { wasRead, body, isFavorite, onNavigateToPost } = this.props
 
     return (
       <SwipeRow
+        ref={(ref) => (this.cardRef = ref)}
         style={styles.card}
         disableRightSwipe
+        recalculateHiddenLayout
+        closeOnRowPrcloseOnRowPressess
         rightOpenValue={-75}
         right={
-          <Button danger onPress={() => alert('Trash')}>
+          <Button danger onPress={this.onDeletePost.bind(this)}>
             <Icon active name="trash" />
           </Button>
         }
         body={
           <View style={styles.content}>
             {wasRead || <View style={styles.dot} />}
-            <Text style={styles.text}>{body.replace(/(\r\n|\n|\r)/gm, ' ')}</Text>
+            <Text style={styles.text} onPress={onNavigateToPost}>
+              {body.replace(/(\r\n|\n|\r)/gm, ' ')}
+            </Text>
             {isFavorite && <Icon style={styles.star} name="star" type="Ionicons" />}
           </View>
         }
@@ -33,12 +49,16 @@ Component.propTypes = {
   wasRead: PropTypes.bool,
   body: PropTypes.string,
   isFavorite: PropTypes.bool,
+  onNavigateToPost: PropTypes.func,
+  onDeletePost: PropTypes.func,
 }
 
 Component.defaultProps = {
   wasRead: false,
   body: '',
   isFavorite: false,
+  onNavigateToPost: () => {},
+  onDeletePost: () => {},
 }
 
 export default Component
