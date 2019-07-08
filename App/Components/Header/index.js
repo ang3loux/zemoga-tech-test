928a5f1b90ebf3c70c130b44067bf34b3bef36cc
Angelo Zambrano <angelo.zambrano.1990@gmail.com>
2019-07-08 18:15:22 -0500

    new header with steroids

--- App/Components/Header/index.js
+++ App/Components/Header/index.js
@@ -1,32 +1,51 @@
 import React from 'react'
 import { Header, Title, Button, Icon, Left, Right, Body } from 'native-base'
+import { PropTypes } from 'prop-types'
+import NavigationService from 'App/Services/NavigationService'
 import styles from './styles'
 import Colors from 'App/Theme/Colors'
 
-class Component extends React.Component {
+class Component extends React.PureComponent {
   render() {
+    const { LeftCustomComponent, showBackButton, title, RightCustomComponent } = this.props
+
     return (
       <Header
-        style={styles.header}
+        style={styles.headerColor}
         androidStatusBarColor={Colors.darkPrimary}
         iosBarStyle="light-content"
+        noLeft={!LeftCustomComponent && !showBackButton}
       >
         <Left>
-          <Button transparent onPress={() => {}}>
-            <Icon name="arrow-back" />
-          </Button>
+          {LeftCustomComponent && <LeftCustomComponent />}
+
+          {showBackButton && (
+            <Button transparent onPress={NavigationService.goBack}>
+              <Icon name="arrow-back" />
+            </Button>
+          )}
         </Left>
+
         <Body>
-          <Title>Header</Title>
+          <Title>{title}</Title>
         </Body>
-        <Right>
-          <Button transparent onPress={() => {}}>
-            <Icon name="menu" />
-          </Button>
-        </Right>
+
+        <Right>{RightCustomComponent && <RightCustomComponent />}</Right>
       </Header>
     )
   }
+}
+
+Component.propTypes = {
+  showBackButton: PropTypes.bool,
+  LeftCustomComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.node, PropTypes.element]),
+  title: PropTypes.string,
+  RightCustomComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.node, PropTypes.element]),
+}
+
+Component.defaultProps = {
+  showBackButton: false,
+  title: ' ',
 }
 
 export default Component
--- App/Components/Header/styles.js
+++ App/Components/Header/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 import Colors from 'App/Theme/Colors'
 
 export default StyleSheet.create({
-  header: {
+  headerColor: {
     backgroundColor: Colors.primary,
   },
 })
